1c7aeabf27fde4d7a1999985527e195f1af0fe85
HYOBIN <hbin12212@gmail.com>
2024-01-13 20:59:18 +0900

    [STYLE] : change styles

--- _site/assets/js/search.js
+++ _site/assets/js/search.js
@@ -1,45 +1,53 @@
+var sjs = SimpleJekyllSearch({
+    searchInput: document.getElementById('searchbar'),
+    resultsContainer: document.getElementById('results-container'),
+    json: '/search.json',
+});
+
 window.onload = function () {
     var $searchbar = document.getElementById('searchbar');
-    var $searchResults = document.getElementById('search-results');
+    var $searchResults = document.getElementById('results-container');
 
-    if (!$searchbar || !$searchResults)
-        return;
+    if (!$searchbar || !$searchResults) return;
 
     SimpleJekyllSearch({
         searchInput: $searchbar,
         resultsContainer: $searchResults,
-        json: '/search.json',
+        json: '{{ "/search.json" | relative_url }}',
         searchResultTemplate: '<a href="{url}" target="_blank">{title}</a>',
-        noResultsText: ''
+        noResultsText: '',
     });
 
     /* hack ios safari unfocus */
     if (/Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent))
         document.body.firstElementChild.tabIndex = 1;
 
-    var $labelGroup = document.querySelector(".posts-labelgroup");
-    var $postLabel = document.getElementById("posts-label");
+    var $labelGroup = document.querySelector('.posts-labelgroup');
+    var $postLabel = document.getElementById('posts-label');
     var labelWidth = $postLabel.scrollWidth;
 
-    $postLabel.style.width = labelWidth + "px";
+    $postLabel.style.width = labelWidth + 'px';
 
-    $labelGroup.addEventListener("click", function (e) {
-        $searchResults.style.display = null;
-        $postLabel.style.width = "0";
-        $labelGroup.setAttribute("class", "posts-labelgroup focus-within");
-        $searchbar.focus();
-        e.stopPropagation();
-    }, false);
+    $labelGroup.addEventListener(
+        'click',
+        function (e) {
+            $searchResults.style.display = null;
+            $postLabel.style.width = '0';
+            $labelGroup.setAttribute('class', 'posts-labelgroup focus-within');
+            $searchbar.focus();
+            e.stopPropagation();
+        },
+        false
+    );
 
-    $labelGroup.addEventListener("mouseleave", function () {
+    $labelGroup.addEventListener('mouseleave', function () {
         document.body.onclick = searchCollapse;
     });
 
     var searchCollapse = function (e) {
-        $searchResults.style.display = "none";
-        $labelGroup.setAttribute("class", "posts-labelgroup");
-        $postLabel.style.width = labelWidth + "px";
+        $searchResults.style.display = 'none';
+        $labelGroup.setAttribute('class', 'posts-labelgroup');
+        $postLabel.style.width = labelWidth + 'px';
         document.body.onclick = null;
     };
-}
-
+};
